97f4e46f11a86ec436470483654bec1704a465ed
krishna0514 <113449814+krishna0514@users.noreply.github.com>
2023-08-10 09:31:34 +0530

    Add files via upload

---
+++ 1/1.passport automation system/Passport_Mangement_System.java
@@ -0,0 +1,86 @@
+
+
+/**
+ * Class Passport_Mangement_System
+ */
+public class Passport_Mangement_System {
+
+  //
+  // Fields
+  //
+
+  private void verify_details;
+  private void store_proof;
+  
+  //
+  // Constructors
+  //
+  public Passport_Mangement_System () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verify_details
+   * @param newVar the new value of verify_details
+   */
+  private void setVerify_details (void newVar) {
+    verify_details = newVar;
+  }
+
+  /**
+   * Get the value of verify_details
+   * @return the value of verify_details
+   */
+  private void getVerify_details () {
+    return verify_details;
+  }
+
+  /**
+   * Set the value of store_proof
+   * @param newVar the new value of store_proof
+   */
+  private void setStore_proof (void newVar) {
+    store_proof = newVar;
+  }
+
+  /**
+   * Get the value of store_proof
+   * @return the value of store_proof
+   */
+  private void getStore_proof () {
+    return store_proof;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void issue_of_passport()
+  {
+  }
+
+
+  /**
+   */
+  public void verification_of_passport()
+  {
+  }
+
+
+  /**
+   */
+  public void cancellation_of_passport()
+  {
+  }
+
+
+}
